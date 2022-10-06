cac9b3de5980c58ec5d6141ee39d3ad8ae1909cd
babar.spyresync <babar.spyresync@gmail.com>
2022-10-06 18:30:25 +0500

    views

--- agora/serializers.py
+++ agora/serializers.py
@@ -1,1 +1,14 @@
 from rest_framework import serializers
+from .models import Companies, Advocates
+
+
+class AdvocatesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Advocates
+        fields = '__all__'
+
+
+class CompaniesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Companies
+        fields = '__all__'
--- agora/views.py
+++ agora/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework import viewsets
+from .models import Companies, Advocates
+from .serializers import CompaniesSerializer, AdvocatesSerializer
+from rest_framework.response import Response
+
 
 # Create your views here.
+
+class AdvocatesViewSets(viewsets.ModelViewSet):
+    queryset = Advocates.objects.all()
+    serializer_class = AdvocatesSerializer
+
+
+class CompaniesViewSets(viewsets.ModelViewSet):
+    queryset = Companies.objects.all()
+    serializer_class = CompaniesSerializer
