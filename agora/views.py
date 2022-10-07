b586ab130843585dd0756729ad044e1e35814a62
babar.spyresync <babar.spyresync@gmail.com>
2022-10-07 16:25:27 +0500

    exception handling

--- agora/views.py
+++ agora/views.py
@@ -17,7 +17,9 @@ class AdvocatesViewSets(viewsets.ModelViewSet):
             serializer = self.serializer_class(data=request.data)
             if serializer.is_valid():
                 serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
+                return Response(
+                    {'message': 'Advocate created successfully', 'data': serializer.data},
+                    status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
@@ -26,7 +28,9 @@ class AdvocatesViewSets(viewsets.ModelViewSet):
         try:
             queryset = self.queryset
             serializer = self.serializer_class(queryset, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(
+                {'message': 'Advocates List successfully', 'data': serializer.data},
+            )
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -34,7 +38,9 @@ class AdvocatesViewSets(viewsets.ModelViewSet):
         try:
             instance = self.get_object()
             serializer = RetrieveAdvocatesSerializer(instance)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(
+                {'message': 'Advocate retrieved successfully', 'data': serializer.data},
+            )
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -44,23 +50,38 @@ class CompaniesViewSets(viewsets.ModelViewSet):
     serializer_class = CompaniesSerializer
 
     def create(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            serializer = self.serializer_class(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    {'message': 'Company created successfully', 'data': serializer.data},
+                    status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
     def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = self.serializer_class(queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        try:
+            queryset = self.queryset
+            serializer = self.serializer_class(queryset, many=True)
+            return Response(
+                {'message': 'Companies List successfully', 'data': serializer.data},
+            )
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
     def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        advocates = Advocates.objects.filter(company=instance)
-        serializer = self.serializer_class(instance)
-        advocates_serializer = CompanyAdvocatesSerializer(advocates, many=True)
-        data = serializer.data
-        data["advocates"] = advocates_serializer.data
-        return Response({'company': data},
-                        status=status.HTTP_200_OK)
+        try:
+            instance = self.get_object()
+            advocates = Advocates.objects.filter(company=instance)
+            serializer = self.serializer_class(instance)
+            advocates_serializer = CompanyAdvocatesSerializer(advocates, many=True)
+            data = serializer.data
+            data["advocates"] = advocates_serializer.data
+            return Response(
+                {'message': 'Company retrieved successfully', 'data': data},
+            )
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
