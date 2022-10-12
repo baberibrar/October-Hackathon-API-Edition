61107bc8be869563ab7d8b905cb6ddfa9e9c1a06
babar.spyresync <babar.spyresync@gmail.com>
2022-10-12 12:09:12 +0500

    'href': reverse

--- agora/serializers.py
+++ agora/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Companies, Advocates
+from django.urls import reverse
 
 
 class AdvocatesSerializer(serializers.ModelSerializer):
@@ -20,7 +21,8 @@ class RetrieveAdvocatesSerializer(serializers.ModelSerializer):
             'id': company_data[0]['id'],
             'name': company_data[0]['name'],
             'logo': company_data[0]['logo'],
-            'href': 'http://web-production-2772.up.railway.app/companies/' + str(company_data[0]['id']) + '/'
+            # 'href': 'http://web-production-2772.up.railway.app/companies/' + str(company_data[0]['id']) + '/'
+            'href': reverse('companies-detail', args=[company_data[0]['id']])
         }
         data['links'] = {
             'youtube': instance.youtube,
